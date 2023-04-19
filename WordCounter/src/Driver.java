d24321b5eff3afac8a93dc885fa794cfbe5ffa76
Bae Sung Kim <baesungkim1999@gmail.com>
2023-04-18 15:50:20 -1000

    Basic file reader.

--- WordCounter/src/Driver.java
+++ WordCounter/src/Driver.java
@@ -1,8 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class Driver {
 
 	public static void main(String[] args) {
-		System.out.println("This is a test.");
+		Path inputFile = Paths.get(args[0]);
+		String line;
+		
+		try (BufferedReader br = Files.newBufferedReader(inputFile)) {
+			while ((line = br.readLine()) != null) {
+				System.out.println(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 }
